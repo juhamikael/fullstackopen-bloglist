4a68ee819f280f1f3d33fd4d286b241868001480
juhamikael <83360104+juhamikael@users.noreply.github.com>
2024-05-03 01:49:22 +0300

    Update testing.js

--- backend/controllers/testing.js
+++ backend/controllers/testing.js
@@ -3,7 +3,6 @@ const Blog = require("../models/blogs");
 const User = require("../models/user");
 
 router.post("/reset", async (request, response) => {
-  console.log("resetting database");
   try {
     await Blog.deleteMany({});
     await User.deleteMany({});

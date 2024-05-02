5fe14fb9cc8e9c0350fb7f60c7b41dc84fef2a35
juhamikael <83360104+juhamikael@users.noreply.github.com>
2024-05-03 01:49:14 +0300

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,7 @@ const middleware = require("./utils/middleware");
 const blogRouter = require("./controllers/blogs");
 const loginRouter = require("./controllers/login");
 const userRouter = require("./controllers/users");
-
+const testingRouter = require("./controllers/testing");
 const connectDB = require("./database/db");
 require("express-async-errors");
 
@@ -18,10 +18,6 @@ app.use(middleware.tokenExtractor);
 app.use("/api/blogs", blogRouter);
 app.use("/api/users", userRouter);
 app.use("/api/login", loginRouter);
-
-if (process.env.NODE_ENV === "test") {
-  const testingRouter = require("./controllers/testing");
-  app.use("/api/testing", testingRouter);
-}
+app.use("/api/testing", testingRouter);
 
 module.exports = app;

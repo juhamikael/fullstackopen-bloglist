630703f9f5e667be8d451ec9a3932964f88d5e22
juhamikael <83360104+juhamikael@users.noreply.github.com>
2024-05-02 21:04:01 +0300

    Auto Format

--- backend/controllers/testing.js
+++ backend/controllers/testing.js
@@ -1,12 +1,17 @@
-const router = require('express').Router()
-const Blog = require('../models/blogs')
-const User = require('../models/user')
+const router = require("express").Router();
+const Blog = require("../models/blogs");
+const User = require("../models/user");
 
-router.post('/reset', async (request, response) => {
-  await Blog.deleteMany({})
-  await User.deleteMany({})
+router.post("/reset", async (request, response) => {
+  console.log("resetting database");
+  try {
+    await Blog.deleteMany({});
+    await User.deleteMany({});
+  } catch (error) {
+    console.log(error);
+  }
 
-  response.status(204).end()
-})
+  response.status(204).end();
+});
 
-module.exports = router
+module.exports = router;
